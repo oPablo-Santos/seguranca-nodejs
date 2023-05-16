54493bac3bec32eecda5e0690c595a2a86781240
oPablo-Santos <pablo_p9_santos@hotmail.com>
2023-05-16 17:58:53 -0300

    update

---
+++ api/migrations/20230516164300-create-usuarios-roles.js
@@ -0,0 +1,44 @@
+"use strict";
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable("usuarios_roles", {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      usuario_id: {
+        type: Sequelize.UUID,
+        references: {
+          model: "usuario",
+          key: "id",
+        },
+        onDelete: "CASCADE",
+        onUpdate: "CASCADE",
+        S,
+      },
+      role_id: {
+        type: Sequelize.UUID,
+        references: {
+          model: "roles",
+          key: "id",
+        },
+        onDelete: "CASCADE",
+        onUpdate: "CASCADE",
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable("usuarios_roles");
+  },
+};
